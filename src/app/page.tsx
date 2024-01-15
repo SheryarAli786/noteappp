a7e4715828ed7045fbb9491ac54f314574b576ff
SheryarAli786 <sheheryarali199@gmail.com>
2024-01-15 15:32:44 +0500

    changed function to arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import styles from './page.module.css';
 import Header from './components/header/Header';
 import NotesComponent from './components/Notes/NotesComponent';
-export default function Home() {
+import React from 'react'
+
+const Home = () => {
   return (
-   <>
-   <Header/>
+    <div>
+       <Header/>
    <NotesComponent/>
-   </>
+    </div>
   )
 }
+
+export default Home

bcd736b58989aa3b66d6e45e0cea1efd9cebd36d
SheryarAli786 <sheheryarali199@gmail.com>
2024-01-15 15:26:33 +0500

    Created Notecomponent write code to save and retrieve data from local storage

---
+++ src/app/components/Notes/NotesComponentStyle.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Container=styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: start;
+    position: relative;
+`
+export const MainContainer=styled.div`
+display: flex;
+flex-direction: column;
+   height: 250px;
+    width: 280px;
+    position: relative;
+    margin-top: 5px;
+    margin-left: 10px;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    h1{
+        font-weight: 400;
+        font-family: 'Roboto', sans-serif;
+    }
+    p{ 
+        font-size: 15px;
+        font-family: 'Roboto', sans-serif;
+    }
+`
+export const ParaContainer=styled.div`
+    height: 250px;
+`
+export const DateContainer=styled.div`
+    display: flex;
+    div{
+        display: flex;
+        gap: 10px;
+        margin-left: auto;
+    }
+    img{ 
+        background-color: black;
+        height: 20px;
+        padding: 10px;
+        width: 20px;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+`;
+export const CloseIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: -11px;
+  right: -6px;
+  opacity: 0; 
+  transition: opacity 0.3s ease; 
+  cursor: pointer;
+
+  &:hover {
+    opacity: 1; 
+  }
+`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import styles from './page.module.css'
-import Header from './components/header/Header'
-
+import styles from './page.module.css';
+import Header from './components/header/Header';
+import NotesComponent from './components/Notes/NotesComponent';
 export default function Home() {
   return (
    <>
    <Header/>
+   <NotesComponent/>
    </>
   )
 }
